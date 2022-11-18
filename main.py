7610a57c9363a839f324b2b1db8174c14f4f2cd5
Swostika <swostikaa99@gmail.com>
2022-11-18 07:06:36 +0545

    finished main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import random
-randNumber = random.randint(0,9)
-print(randNumber)
-
-yourGuess = int(input("Enter the number of your guess:  "))
-if(yourGuess ==randNumber):
+randNumber = random.randint(1,99)
+yourGuess = None
+guesses = 0
+while(yourGuess != randNumber):
+  yourGuess = int(input("Enter the two digit number of your guess:  "))
+  guesses  += 1
+  if(yourGuess ==randNumber):
     print("You guessed it right!")
-else:
-    print("you")
+  else:
+        if(yourGuess>randNumber):
+         print("You guessed it wrong! Enter a smaller number")
+        else:
+              print("You guessed it wrong! Enter a larger number")
+    
+print(f"You guessed the number in {guesses} guesses")
+with open("highscore.txt", "r") as f:
+  highscore = int(f.read())
+
+if(guesses<highscore): 
+  print("Congratulations! you just achieved a highscore")
+  with open("highscore.txt", "w") as f:
+
+   f.write('%d' %guesses)
